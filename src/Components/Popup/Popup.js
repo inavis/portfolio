e75c944697f4e6da083bce96693f5d45e4efb4ee
inavis <rednaz.inavis@gmail.com>
2025-03-06 15:38:26 +0530

    popup

---
+++ src/Components/Popup/Popup.js
@@ -0,0 +1,28 @@
+import "./Popup.css";
+import { StateContext } from "../../Context";
+import { useContext } from "react";
+import closeIcon from "../../Images/cross.png";
+
+function Popup({ children }) {
+  let { popupOpen, closePopup } = useContext(StateContext);
+  return (
+    <div className={`popup-template ${popupOpen ? "active" : ""}`}>
+      <div className="overlay"></div>
+      <div className="modal">
+        <div className="modal-content">
+          <div className="w-100">
+            <img
+              className="close"
+              src={closeIcon}
+              alt="close"
+              onClick={closePopup}
+            />
+          </div>
+          <h1>{children}</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Popup;
