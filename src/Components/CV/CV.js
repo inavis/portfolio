62868e7a44660d76997f4abbfa73165db2acad87
inavis <rednaz.inavis@gmail.com>
2025-03-06 15:38:04 +0530

    download-cv

---
+++ src/Components/CV/CV.js
@@ -0,0 +1,22 @@
+import "./CV.css";
+import Button from "../Button/Button";
+import RightArrow from "../../Images/right-arrow-1.png";
+import { Link } from "react-router-dom";
+import resume from "../../Pdf/Sivani-EU-resume.pdf";
+
+function CV() {
+  return (
+    <section id="cv" className="d-flex w-100">
+      <Link to={resume} target="_blank" download={"Sivani-Resume"}>
+        <Button
+          text={"Download CV"}
+          iconPath={RightArrow}
+          iconAlt={"arrow"}
+          customClass={"text-center-tab w-100"}
+        />
+      </Link>
+    </section>
+  );
+}
+
+export default CV;
